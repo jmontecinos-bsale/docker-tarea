8e05aaefe4ff3eda6d98f4a4ad6c14a470f796c4
jmontecinos-bsale <jmontecinos@imaginex.cl>
2026-08-18 10:30:40 -0400

    añadir env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const PORT = process.env.SERVER_PORT || 3000; 
 
 app.use(cors());
 
@@ -8,4 +9,4 @@ app.get('/docker-api', (req, res) => {
     res.json({msg: '¡Hola! Saludos desde el backend :)'})
 });
 
-app.listen(3000, () => console.log('Backend running on port 3000'));
+app.listen(SERVER_PORT, () => console.log(`Backend running on port ${SERVER_PORT}`));
